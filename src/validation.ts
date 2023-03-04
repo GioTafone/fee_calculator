ab4835e2fed5842688a570efbc788eb52249a6e8
Giovanni Tafone <giola@Giovannis-MacBook-Air.local>
2023-03-04 18:55:14 +0200

    changed validation messages

--- src/validation.ts
+++ src/validation.ts
@@ -3,17 +3,17 @@ import { z } from "zod";
 export const inputFeeSchema = z.object({
   cartValue: z
     .number()
-    .positive({ message: "Cart value must be a positive number" })
+    .positive({ message: "Cart value <cannot be a negative number>" })
     .min(0.01, { message: "Insert cart value" })
     .multipleOf(0.01, {message: "Partials of € cents non accepted"}),
   distance: z
     .number()
-    .positive({ message: "Distance must be a positive number" })
+    .positive({ message: "Distance cannot be a negative number" })
     .int({ message: "Only integers numbers are valid" })
     .max(20000, { message: "Distance must be less than 20000 meters (20Km)" }),
   itemsCount: z
     .number()
-    .positive({ message: "Number of items must be a positive number" })
+    .positive({ message: "Number of items cannot be a negative number" })
     .int({ message: "Only integers numbers are valid" })
     .min(1, { message: "Insert number of items" })
     .max(500, { message: "Number of items must be below 500" }),
